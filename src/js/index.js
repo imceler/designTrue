a6ec973cc6a03545cfa9b1bcdb2de9b41f3d80a1
Gustavo Cañas <ghenriquez700@gmail.com>
2019-08-10 13:05:28 -0400

    details

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const pricesBtn = document.querySelector('#prices')  
+const back = document.querySelector('.background')
+const btn = document.querySelector('.button')
+const box = document.querySelector('.login-box')
+
+// const tl = new TimelineMax();
+
+// btn.addEventListener('click', () => {
+//     tl.fromTo(
+//         box, 
+//         1, 
+//         { background: '' }, 
+//         {  , 
+//         ease: back.easeInOut })
+// })
+
