a1cd0c9167eeea229ff433e66a42a5c800256f1e
Gustavo Cañas <ghenriquez700@gmail.com>
2019-08-10 19:24:55 -0400

    First stage of animations is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
-const pricesBtn = document.querySelector('#prices')  
-const back = document.querySelector('.background')
-const btn = document.querySelector('.button')
-const box = document.querySelector('.login-box')
+const arrowBack = document.querySelector('.arrow-back img')
+const tl = new TimelineMax();
 
-// const tl = new TimelineMax();
+tl.fromTo('.table', .6, { x: '0'}, { x: '100px',  ease: Power1.easeInOut})
+.fromTo('.table', .8, { height: '10%' }, { height: '100%', ease: Power1.easeInOut })
+.fromTo('.table-body', 1.2, { opacity: '.3'}, { opacity: '1', ease: Power1.easeInOut}, '-=1')
+.fromTo('.table-body div', 2, { opacity: '0'}, { opacity: '1', ease: Power1.easeInOut }, '-=1')
+.fromTo('.table-body button', 1, { opacity: '0'}, { opacity: '1', ease: Power1.easeInOut}, '-=2')
+.fromTo('.table-body div', 1, {display: 'none'}, {display: 'block', ease: Power1.easeInOut }, '-=4')
+.fromTo('.table-body button', 1, {display: 'none'}, {display: 'block', ease: Power1.easeInOut }, '-=4')
 
-// btn.addEventListener('click', () => {
-//     tl.fromTo(
-//         box, 
-//         1, 
-//         { background: '' }, 
-//         {  , 
-//         ease: back.easeInOut })
-// })
-
+var backHome = () => {
+    location.replace("./index.html");
+}
+arrowBack.addEventListener('click', () => {
+    backHome()
+})
